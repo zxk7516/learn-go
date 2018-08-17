99312b6259bf26d54dc37c2c9a4c18cf982e0e51
zxk7516 <zxk7516@foxmail.com>
2018-08-17 20:25:31 +0800

    charset.

---
+++ crawler/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"regexp"
+)
+
+func getHtmlContents(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		panic("status error")
+	}
+	e := determineEncoding(resp.Body)
+	// utf8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	return all
+}
+func main() {
+	url := "http://www.zhenai.com/zhenghun"
+	zhenhun := getHtmlContents(url)
+	printCityList(zhenhun)
+
+}
+
+func determineEncoding(
+	r io.Reader,
+) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+func printCityList(contents []byte) {
+	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		//for _,subMatch := range m{
+		//	fmt.Printf("%s ",subMatch)
+		//}
+		fmt.Printf("%s -> %s\n", m[2], m[1])
+	}
+	fmt.Printf("Matched found: %d\n", len(matches))
+}
+
+func getGirsList(city, url string) {
+	list_html := getHtmlContents(url)
+}
